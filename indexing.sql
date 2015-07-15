a9ffe7557e9b61d990cd0c9cd6f999035555e051
Judah Lamm <Judah.Lamm@gmail.com>
2015-07-15 11:57:34 -1000

    setup complete

---
+++ indexing.sql
@@ -0,0 +1,15 @@
+CREATE USER indexed_cars_user;
+CREATE DATABASE indexed_cars OWNER indexed_cars_user ENCODING 'utf8';
+\i ~/Devleague/Automotive-Index/scripts/car_models.sql
+--run the below code 10 times! being done manually
+\i ~/Devleague/Automotive-Index/scripts/car_model_data.sql
+\i ~/Devleague/Automotive-Index/scripts/car_model_data.sql
+\i ~/Devleague/Automotive-Index/scripts/car_model_data.sql
+\i ~/Devleague/Automotive-Index/scripts/car_model_data.sql
+\i ~/Devleague/Automotive-Index/scripts/car_model_data.sql
+\i ~/Devleague/Automotive-Index/scripts/car_model_data.sql
+\i ~/Devleague/Automotive-Index/scripts/car_model_data.sql
+\i ~/Devleague/Automotive-Index/scripts/car_model_data.sql
+\i ~/Devleague/Automotive-Index/scripts/car_model_data.sql
+\i ~/Devleague/Automotive-Index/scripts/car_model_data.sql
+
